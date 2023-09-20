5e1aadb2b535db96ed8ceb303cf78ad41b3b39f3
Bhim Rai <bhimrai@Bhims-MacBook-Pro.local>
2023-09-20 22:45:33 +0545

    Friend Function to withdraw money maintaining proper validation

---
+++ 3.FriendFunctionWithdrawMoney.cpp
@@ -0,0 +1,32 @@
+//3. Friend function(withdraw) to withdraw the amount.
+#include<iostream>
+using namespace std;
+class BankAccount{
+    private:
+        int balance;
+        int amt;
+    public:
+        void getdata()
+        {
+            cout<<endl<<"Enter amount to withdraw: ";
+            cin>>amt;
+        }
+    friend void withdraw(BankAccount);
+    BankAccount()
+    {
+        balance=15000;
+        cout<<"Your Balance: "<<balance;
+    }
+}amount;
+void withdraw(BankAccount temp)
+{
+    cout<<endl<<"The withdrawn amount is: "<<temp.amt;
+    cout<<endl<<"Balance Left: "<<temp.balance-temp.amt;
+}
+int main()
+{
+    //BankAccount amount;
+    amount.getdata();
+    withdraw(amount);
+    return 0;
+}
---
+++ 5.cpp
@@ -0,0 +1,8 @@
+//
+//  5.cpp
+//  
+//
+//  Created by Bhim Rai on 9/20/23.
+//
+
+#include <stdio.h>
