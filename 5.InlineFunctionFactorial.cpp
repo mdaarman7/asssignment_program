e2ae0a1a64729b9c8c1d0047afe792a0f57f4a27
Bhim Rai <bhimrai@Bhims-MacBook-Pro.local>
2023-09-20 22:47:34 +0545

    Inline Function Factorail Added

---
+++ 5.InlineFunctionFactorial.cpp
@@ -0,0 +1,22 @@
+//1. (inline Function) Factorial of a number.
+#include<iostream>
+using namespace std;
+inline int fact(int m)
+{
+    int res=1;
+    while(m>0)
+    {
+        res=res*m;
+        m--;
+    }
+    return res;
+}
+int main()
+{
+    int factorial=0,num=0;
+    cout<<"Enter a number for factorial: ";
+    cin>>num;
+    factorial=fact(num);
+    cout<<endl<<"The Factorial of "<<num<< " is: "<<factorial;
+    return 0;
+}
